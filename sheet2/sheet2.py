9eb6bba7ad4d5095303a2cf1de1c15b017f4981c
faggl <m.4esfd@gmail.com>
2023-04-27 15:52:01 +0200

    Fehlerhaftes backward scheme

--- sheet2/sheet2.py
+++ sheet2/sheet2.py
@@ -1,8 +1,9 @@
 import numpy as np
 import matplotlib.pyplot as plt
+from scipy.linalg import lu
 
 def boundary_cond(x,L):
-    if 0<x<=(L/2):
+    if x<=(L/2):
         y = 2*x
     else:
         y = 2*(L - x)
@@ -55,10 +56,36 @@ def fd_backward_heat(Nx, Nt, T, L, sigma):
     delta_t = T / Nt
     delta_x = L / Nx
     nu = (sigma**2)*(delta_t/(delta_x**2))
-    u = np.zeros((Nt+1, Nx+1))
+    u = np.zeros((Nx, Nt))
     
-    B = np.zeros((range(Nx), range(Nx)))
+    # Matrix B allocation
+    B = np.zeros((Nx, Nx))
+    for i in range(Nx):
+        B[i,i] = 1-2*nu
+        
+    for i in range(Nx-1):
+        B[i+1,i] = nu
+        B[i,i+1] = nu
     
+    # L U Decomposition, Jetzt "R-U Zerlegung", da L obere Grenze vom State space ist
+    U, R = lu(B,permute_l=True)
+    U_inv = np.linalg.inv(U)
+    R_inv = np.linalg.inv(R)
+    
+    # 
+    qi = nu*np.zeros(shape=(1,Nt))
+    
+    # Allocate U_0
+    for j in range(1, Nx):
+        u[0,j] = boundary_cond(j*delta_x,L)
+        
+    # Rekursion
+    for i in range(Nt):  
+        y = U_inv.dot(u[i,:])
+        u[i+1,:] = R_inv.dot(y)
+    return u
+    
+       
 
 
 Nx = 14
@@ -67,12 +94,15 @@ T = 3
 L = np.pi
 t = np.linspace(0,T,Nt+1)
 x = np.linspace(0,L,Nx+1)
-u = fd_forward_heat(Nx= Nx, Nt= Nt, T = T, L = L, sigma= 1)
+u1 = fd_forward_heat(Nx= Nx, Nt= Nt, T = T, L = L, sigma= 1)
+
+u2 = fd_backward_heat(Nx, Nt, T, L, sigma = 1)
+
 
 y, z = np.meshgrid(x,t)
 fig = plt.figure()
 ax = plt.axes(projection='3d')
-ax.contour3D(y, z, u, 1000)
+ax.contour3D(y, z, u1, 1000)
 ax.set_xlabel('t')
 ax.set_ylabel('x')
 ax.set_zlabel('u')
