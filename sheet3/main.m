e21a4fe89bfc2793708897a44263d523222f55e5
faggl <m.4esfd@gmail.com>
2023-05-05 16:07:20 +0200

    Update main.m

--- sheet3/main.m
+++ sheet3/main.m
@@ -6,5 +6,10 @@ r = 0.03;
 K = 110;
 sigma = 0.2;
 L = 200;
+delta_t = T/Nt;
+delta_s = L/Ns;
 
 X = crank_nicolson(Nt,Ns,T,L,sigma,r,K);
+
+[Y, Z] = meshgrid(0:delta_t:T, 0:delta_s:L);
+surfc(Y,Z,X)
